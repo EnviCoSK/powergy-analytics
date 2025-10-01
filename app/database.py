30ffe9132c453c7d1ef9aa19c8592b22dd10df40
EnviCoSK <bruno.lipnicka@envico.sk>
2025-10-01 14:50:52 +0200

    database.py

--- app/database.py
+++ app/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 from .settings import DATABASE_URL
 from .models import Base
@@ -7,4 +7,10 @@ engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 def init_db():
+    # vytvorí tabuľky ak neexistujú
     Base.metadata.create_all(bind=engine)
+
+    # vytvorí index ak neexistuje
+    with engine.connect() as conn:
+        conn.execute(text("CREATE INDEX IF NOT EXISTS idx_gsd_date ON gas_storage_daily(date);"))
+        conn.commit()
